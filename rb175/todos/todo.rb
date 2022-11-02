6ef04e6c419175dc20fd23e1fa0712e3156d80dd
Max Tan <yenseow@gmail.com>
2022-11-02 15:19:48 -0400

    delete lists

--- rb175/todos/todo.rb
+++ rb175/todos/todo.rb
@@ -21,13 +21,46 @@ get '/lists' do
   erb :lists, layout: :layout
 end
 
+# View a specific list
+
+get '/lists/:index' do
+  idx =  params[:index].to_i
+  @list = session[:lists][idx]
+  erb :list, layout: :layout
+end
+
 # Render the new list form
 get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
+# Clears all lists
+get '/lists/clear' do
+  session[:lists] = []
+  redirect '/lists'
+end
+
+# Return an error message if the name is invalid.
+# Return nil if name is valid.
+
+def error_for_list_name(name)
+  if !(1..100).cover? name.length # use cover? instead of include?
+    'The list name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'The list name must be unique.'
+  end
+end
+
 # Create a new list
 post '/lists' do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  redirect '/lists'
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
 end
