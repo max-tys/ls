f3407fb583bf4fa983b3347ebd91e9fe055fde8b
Max Tan <yenseow@gmail.com>
2022-11-02 08:34:27 -0400

    create new lists

--- rb175/todos/todo.rb
+++ rb175/todos/todo.rb
@@ -1,7 +1,33 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/' do
+  redirect '/lists'
+end
+
+# View all the lists
+get '/lists' do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get '/lists/new' do
+  erb :new_list, layout: :layout
+end
+
+# Create a new list
+post '/lists' do
+  session[:lists] << { name: params[:list_name], todos: [] }
+  redirect '/lists'
 end
