0801b6281da8eb3458a9c06eec71d8da52b9ddf4
Max Tan <yenseow@gmail.com>
2022-11-09 22:56:37 -0500

    view files

--- rb175/cms/cms.rb
+++ rb175/cms/cms.rb
@@ -3,7 +3,15 @@ require 'sinatra/reloader' if development?
 # require 'sinatra/content_for'
 require 'tilt/erubis'
 
+root = File.expand_path("..", __FILE__)
+
 get "/" do
-  @documents = Dir.glob("/home/yenseow/ls/rb175/cms/public/documents/*").map{ |name| File.basename(name) }
-  erb :index, layout: :layout
+  @files = Dir.glob(root + "/data/*")
+              .map{ |path| File.basename(path) }
+  erb :index
+end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+  send_file file_path, :type => :txt
 end
