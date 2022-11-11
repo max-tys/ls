169ff210a72aa18ad473faf73306be180293c5f8
Max Tan <yenseow@gmail.com>
2022-11-11 10:38:09 -0500

    initial tests

--- rb175/cms/test/cms_test.rb
+++ rb175/cms/test/cms_test.rb
@@ -14,7 +14,21 @@ class CMSTest < Minitest::Test
 
   def test_index
     get "/"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt"
+  end
+
+  def test_viewing_text_document
+    get "/history.txt"
+    
+    assert_equal 200, last_response.status
+    assert_equal "text/plain;charset=utf-8", last_response["Content-Type"]
+    assert last_response.body.include? "1993 - Yukihiro Matsumoto dreams up Ruby."
+    assert last_response.body.include? "2013 - Ruby 2.0 released."
+    assert last_response.body.include? "2019 - Ruby 2.7 released."
   end
 end
