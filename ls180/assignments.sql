0aa951eaef3f3588badf58a37ece3c564a03c98e
Max Tan <yenseow@gmail.com>
2022-11-28 10:15:40 -0500

    many to many

--- ls180/assignments.sql
+++ ls180/assignments.sql
@@ -129,18 +129,59 @@ ALTER COLUMN category_id SET NOT NULL;
 
 ALTER TABLE books_categories
 DROP CONSTRAINT books_categories_book_id_fkey,
-DROP CONSTRAINT books_categories_category_id_fkey;
-
-ALTER TABLE books_categories
 ADD CONSTRAINT books_categories_book_id_fkey
 FOREIGN KEY (book_id) REFERENCES books(id)
 ON DELETE CASCADE;
 
 ALTER TABLE books_categories
+DROP CONSTRAINT books_categories_category_id_fkey,
 ADD CONSTRAINT books_categories_category_id_fkey
 FOREIGN KEY (category_id) REFERENCES categories(id)
 ON DELETE CASCADE;
 
 -- 2) Write a SQL statement that will return the following result:
--- Continue from here
 
+SELECT b.id, b.author, STRING_AGG(c.name, ', ') AS categories
+  FROM books AS b
+       JOIN books_categories AS bc ON b.id = bc.book_id
+       JOIN categories AS c ON c.id = bc.category_id
+ GROUP BY b.id
+ ORDER BY id;
+
+-- 3) Write SQL statements to insert the following new books into the database. 
+-- What do you need to do to ensure this data fits in the database?
+
+ALTER TABLE books
+ALTER COLUMN title TYPE varchar(80);
+
+INSERT INTO books
+(author, title) VALUES 
+('Lynn Sherr', 'Sally Ride: America''s First Woman in Space'),
+('Charlotte Brontë', 'Jane Eyre'),
+('Meeru Dhalwala and Vikram Vij', 'Vij''s: Elegant and Inspired Indian Cuisine');
+
+INSERT INTO categories
+(name) VALUES
+('Space Exploration'),
+('Cookbook'),
+('South Asia');
+
+INSERT INTO books_categories VALUES
+(4, 5), (4, 1), (4, 7),
+(5, 2), (5, 4),
+(6, 8), (6, 1), (6, 9);
+
+-- 4) Write a SQL statement to add a uniqueness constraint on the combination of columns book_id and category_id of the books_categories table. 
+-- This constraint should be a table constraint; so, it should check for uniqueness on the combination of book_id and category_id across all rows of the books_categories table.
+
+ALTER TABLE books_categories
+ADD UNIQUE (book_id, category_id);
+
+-- 5) Write a SQL statement that will return the following result:
+
+SELECT c.name, COUNT(b.id) AS book_count, STRING_AGG(b.title, ', ') AS book_titles
+  FROM books AS b
+       JOIN books_categories AS bc ON b.id = bc.book_id
+       JOIN categories AS c ON c.id = bc.category_id
+ GROUP BY c.name
+ ORDER BY c.name;
