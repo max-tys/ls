8528b55fe82055722c3e5093dc474e4c184b6341
Max Tan <yenseow@gmail.com>
2022-11-27 23:04:31 -0500

    many to many

--- ls180/assignments.sql
+++ ls180/assignments.sql
@@ -118,3 +118,29 @@ FROM tickets AS t
      JOIN sections  AS s2 ON s1.section_id = s2.id
      JOIN customers AS c  ON t.customer_id = c.id
 WHERE c.email = 'gennaro.rath@mcdermott.co';
+
+-- lesson 3: Relational Data and JOINs
+-- Many-to-Many Relationships
+-- 1) The books_categories table from this database was created with foreign keys that don't have the NOT NULL and ON DELETE CASCADE constraints. Go ahead and add them now.
+
+ALTER TABLE books_categories
+ALTER COLUMN book_id SET NOT NULL,
+ALTER COLUMN category_id SET NOT NULL;
+
+ALTER TABLE books_categories
+DROP CONSTRAINT books_categories_book_id_fkey,
+DROP CONSTRAINT books_categories_category_id_fkey;
+
+ALTER TABLE books_categories
+ADD CONSTRAINT books_categories_book_id_fkey
+FOREIGN KEY (book_id) REFERENCES books(id)
+ON DELETE CASCADE;
+
+ALTER TABLE books_categories
+ADD CONSTRAINT books_categories_category_id_fkey
+FOREIGN KEY (category_id) REFERENCES categories(id)
+ON DELETE CASCADE;
+
+-- 2) Write a SQL statement that will return the following result:
+-- Continue from here
+
