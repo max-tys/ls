450289eb785a33bf3cfb43f5f1e5ab4695a7fafd
Max Tan <yenseow@gmail.com>
2022-12-05 22:15:26 -0500

    postgres setup

--- rb185/todos/schema.sql
+++ rb185/todos/schema.sql
@@ -7,5 +7,5 @@ CREATE TABLE todos (
   id        serial  PRIMARY KEY,
   name      text    NOT NULL,
   completed boolean NOT NULL DEFAULT 'false',
-  list_id   integer NOT NULL REFERENCES lists.id
+  list_id   integer NOT NULL REFERENCES lists(id)
 );
